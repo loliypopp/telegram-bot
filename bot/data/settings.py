87eb7b89def37f532cabbc0616813a6f34cc8f12
curiousmf <129069615+curiousmf@users.noreply.github.com>
2023-11-06 22:40:51 +0500

    'v2'

--- bot/data/settings.py
+++ bot/data/settings.py
@@ -1,10 +1,42 @@
 import psycopg2 as sql
+import aiogram
 
-base = sql.connect(database='onlineshop', user='gaboom', host='localhost', password='0000')
+base = sql.connect(database='onlineshop', 
+                   user='gaboom', 
+                   host='localhost', 
+                   password='0000')
 cursor = base.cursor()
 
 
 
+def get_product_id_by_name(product_name):
+    global base, cursor
+    try:
+        cursor.execute("SELECT id FROM product WHERE title = %s;", (product_name, ))
+        product_id = cursor.fetchone()
+        if product_id:
+            return product_id[0]
+        else:
+            return None
+    except Exception as e:
+        print('Ошибка при получении product_id:', str(e))
 
-class Users:
-    ...
+
+def get_product_by_uuid(uuid):
+    global base, cursor
+    try:
+        cursor.execute("SELECT product.title, product.category, product.price FROM product WHERE id = %s;", (uuid, ))
+        product = cursor.fetchall()
+        return product
+    except Exception as e:
+        print('Ошибка при получении product:', str(e))
+
+
+
+def update_product_by_uuid(uuid, title, category, price):
+    global base, cursor
+    try:
+        cursor.execute(f'UPDATE product SET title=%s, category=%s, price=%s WHERE id=%s;', (title, category, price, uuid))
+        base.commit()
+    except Exception as e:
+        print('Ошибка при обновлении product:', str(e))
--- bot/handlers/bot_messages.py
+++ bot/handlers/bot_messages.py
@@ -2,20 +2,11 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from aiogram import Router
 from aiogram.fsm.context import FSMContext
-from data.settings import base, cursor
+from data.settings import base, cursor, get_product_id_by_name, get_product_by_uuid, update_product_by_uuid
 from aiogram import F
 
+
 router = Router()
-
-
-class Product:
-    def __init__(self, name, category, price):
-        self.name = name
-        self.category = category
-        self.price = price
-
-    def get_data_for_insert_query(self):
-        return (self.name, self.category, self.price)
 
 
 class AddProductForm(StatesGroup):
@@ -24,12 +15,39 @@ class AddProductForm(StatesGroup):
     enter_price = State()
 
 
+class UpdateProductForm(StatesGroup):
+    uuid = State()
+    edit_name = State()
+    edit_category = State()
+    edit_price = State()
+
+
+class FindTables(StatesGroup):
+    enter_product_name= State()
+
+
+''' REQUEST STARTS '''
+# c
 @router.message(F.text.lower()=='добавить продукт')
 async def add_product_script(message: Message, state: FSMContext):
     await state.set_state(AddProductForm.enter_name)
     await message.answer('Сценарий добавления продукта!\nВведи название продукта!')
+# u
+@router.message(F.text.lower()=='обновить продукт')
+async def update_product_script(message: Message, state: FSMContext):
+    await state.set_state(UpdateProductForm.uuid)
+    await message.answer('Сценари изменения продукта!\nВведите название продукта которого вы хотите изменить!')
+
+# r
+@router.message(F.text.lower()=='посмотреть продукт')
+async def product_details_script(message: Message, state: FSMContext):
+    await state.clear()
+    await state.set_state(FindTables.enter_product_name)
+    await message.answer('Введите название продукта, которого\nвы хотите посмотреть!')
 
 
+
+''' CREATION '''
 @router.message(AddProductForm.enter_name)
 async def process_product_name(message: Message, state: FSMContext):
    
@@ -46,9 +64,10 @@ async def process_product_category(message: Message, state: FSMContext):
     await message.answer(
         'Отлично!\nТеперь укажи цену на товар!'
     )
-
 @router.message(AddProductForm.enter_price)
 async def process_commit(message: Message, state: FSMContext):
+
+
     data = await state.update_data(enter_price=int(message.text))
 
     name = data['enter_name']
@@ -61,4 +80,55 @@ async def process_commit(message: Message, state: FSMContext):
 
     cursor.execute(query, value)
     base.commit()
-    await message.answer('Товар добавлен успешно!')
+    await message.answer(f'Товар добавлен успешно!\nTitle: {name} - Category: {category} - Price: {price}')
+
+
+
+''' SHOW PRODUCT(S) '''
+@router.message(FindTables.enter_product_name)
+async def show_product(message: Message, state: FSMContext):
+    data = await state.update_data(enter_product_name=message.text)
+    name =  data['enter_product_name'].capitalize()
+
+    uuid = get_product_id_by_name(name)
+    value = get_product_by_uuid(uuid)
+    print(value)
+    title, category, price = value[0]
+
+    await message.answer(f'Вот данные о продукте\n\nНазвание: {title}\nКатегория:{category}\nЦена:{price}')
+
+
+''' UPDATE PRODUCT '''
+@router.message(UpdateProductForm.uuid)
+async def find_product_procces(message: Message, state: FSMContext):
+    await state.update_data(uuid=message.text)
+    await state.set_state(UpdateProductForm.edit_name)
+    await message.answer('Укажи новый заголовок для товара!')
+
+@router.message(UpdateProductForm.edit_name)
+async def edit_name_proccess(message: Message, state: FSMContext):
+    await state.update_data(edit_name=message.text)
+    await state.set_state(UpdateProductForm.edit_category)
+    await message.answer('Укажи новую категорию товара!')
+
+@router.message(UpdateProductForm.edit_category)
+async def edit_category_proccess(message: Message, state: FSMContext):
+    await state.update_data(edit_category = message.text)
+    await state.set_state(UpdateProductForm.edit_price)
+    await message.answer('Укажи новую цену товара!')
+
+
+@router.message(UpdateProductForm.edit_price)
+async def proccess_commit_update(message: Message, state: FSMContext):
+    data = await state.update_data(edit_price = int(message.text))
+      
+    uuid = data['uuid'].capitalize()
+    uuid = get_product_id_by_name(uuid)
+
+    title,category,price = data['edit_name'], data['edit_category'], data['edit_price'] 
+
+    change = update_product_by_uuid(uuid, title, category, price)
+    
+    print(change)
+    state.clear()
+    await message.answer('Продукт успешно обновлен!')
